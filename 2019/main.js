2f640dbb20bf6347242d28ce6596bf24a2500c82
OFEII <2282252066@qq.com>
2020-01-18 14:42:16 +0800

    demo website from

--- 2019/main.js
+++ 2019/main.js
@@ -2,73 +2,73 @@ const projects =[
 	{
 		nr: '001',
 		name: 'login jump',
-		link: 'https://codepen.io/OFEII/pen/bXGWmj',
+		link: 'https://ofeii.github.io/demo2019/2019/',
 		demoImg:'https://s2.ax1x.com/2020/01/16/lxmmhF.gif'
 	},
 	{
 		nr: '002',
 		name: '3d btn',
-		link: 'https://codepen.io/FlorinPop17/full/eYOPdER',
+		link: 'https://ofeii.github.io/demo-senior/3d-btn/',
 		demoImg:'https://s2.ax1x.com/2020/01/16/lxmiXn.png'
 	},
 	{
 		nr: '003',
 		name: '3d flip btn',
-		link: 'https://codepen.io/FlorinPop17/full/GRKwmgK',
+		link: 'https://ofeii.github.io/demo-senior/btn-hover-gradient/',
 		demoImg:'https://s2.ax1x.com/2020/01/16/lxejTf.png'
 	},
 	{
 		nr: '004',
 		name: 'email',
-		link: 'https://codepen.io/FlorinPop17/full/NWKELoq',
+		link: 'https://ofeii.github.io/demo-senior/hover/',
 		demoImg:'https://s2.ax1x.com/2020/01/16/lxmSfg.png'
 	},
 	{
 		nr: '005',
 		name: '渐变边框',
-		link: 'https://codepen.io/FlorinPop17/full/QWLzdrV',
+		link: 'https://ofeii.github.io/demo-senior/border-gradient/',
 		demoImg:'https://s2.ax1x.com/2020/01/16/lxm9pQ.png'
 	},
 	{
 		nr: '006',
 		name: '登录界面',
-		link: 'https://codepen.io/FlorinPop17/full/gOYZxyE',
+		link: 'https://codepen.io/OFEII/full/BgqxQz',
 		demoImg:'https://s2.ax1x.com/2020/01/16/lxmClj.jpg'
 	},
 	{
 		nr: '007',
 		name: 'box-div',
-		link: 'https://codepen.io/FlorinPop17/full/KKPbjwy',
+		link: 'https://ofeii.github.io/demo-senior/box-div/',
 		demoImg:'https://s2.ax1x.com/2020/01/16/lxmP6s.png'
 	},
 	{
 		nr: '008',
 		name: '3d btn',
-		link: 'https://codepen.io/FlorinPop17/full/aboXgpP',
+		link: 'https://ofeii.github.io/demo-senior/btn-3d/',
 		demoImg:'https://s2.ax1x.com/2020/01/16/lxmA00.png'
 	},
 	{
 		nr: '009',
 		name: 'mobilephone-ad',
-		link: 'https://codepen.io/FlorinPop17/full/rNBRYKZ',
+		link: 'https://codepen.io/OFEII/full/QzezVJ',
 		demoImg:'https://s2.ax1x.com/2020/01/16/lxmup4.png'
 	},
 	{
 		nr: '010',
 		name: '天气',
-		link: 'https://codepen.io/FlorinPop17/full/RwbOwrm',
+		link: 'https://codepen.io/OFEII/full/JwgjBq',
 		demoImg:'https://s2.ax1x.com/2020/01/16/lxmZkT.png'
 	},
 	{
 		nr: '011',
 		name: '购物清单',
-		link: 'https://codepen.io/FlorinPop17/full/JjPVBwx',
+		link: 'https://codepen.io/OFEII/full/qgErjg',
 		demoImg:'https://s2.ax1x.com/2020/01/16/lxmetU.png'
 	},
 	{
 		nr: '012',
 		name: 'btn',
-		link: 'https://codepen.io/FlorinPop17/full/qBWGmqa',
+		link: 'https://codepen.io/OFEII/full/yZewyq',
 		demoImg:'https://s2.ax1x.com/2020/01/16/lxmK1J.png'
 	},
 	{
@@ -80,163 +80,163 @@ const projects =[
 	{
 		nr: '014',
 		name: 'fluteer water',
-		link: 'https://codepen.io/FlorinPop17/full/ExYBNGy',
+		link: 'https://codepen.io/OFEII/full/rPMmEG',
 		demoImg:'https://s2.ax1x.com/2020/01/16/lxm1n1.png'
 	},
 	{
 		nr: '015',
 		name: 'switch btn',
-		link: 'https://codepen.io/FlorinPop17/full/NWKQWmq',
+		link: 'https://codepen.io/OFEII/full/BMzoqw',
 		demoImg:'https://s2.ax1x.com/2020/01/16/lxmYtO.png'
 	},
 	{
 		nr: '016',
 		name: 'dm screen',
-		link: 'https://codepen.io/FlorinPop17/full/eYOqQLz',
+		link: 'https://codepen.io/OFEII/full/xMeEba',
 		demoImg:'https://s2.ax1x.com/2020/01/16/lxm30x.png'
 	},
 	{
 		nr: '017',
 		name: 'color picker',
-		link: 'https://codepen.io/FlorinPop17/full/yLLBPLZ',
+		link: 'https://codepen.io/OFEII/full/zbYaod',
 		demoImg:'https://s2.ax1x.com/2020/01/16/lxm876.png'
 	},
 	{
 		nr: '018',
 		name: 'color-cicrle',
-		link: 'https://codepen.io/FlorinPop17/full/ExxYJdE',
+		link: 'https://codepen.io/OFEII/full/LqNKdE',
 		demoImg:'https://s2.ax1x.com/2020/01/16/lxmJAK.png'
 	},
 	{
 		nr: '019',
 		name: 'diamond',
-		link: 'https://codepen.io/FlorinPop17/full/eYYYErv',
+		link: 'https://codepen.io/OFEII/full/xMgQRX',
 		demoImg:'https://s2.ax1x.com/2020/01/16/lxmthD.png'
 	},
 	{
 		nr: '020',
 		name: 'OFEII Seaarch in wiki',
-		link: 'https://codepen.io/FlorinPop17/full/NWWWYoe',
+		link: 'https://codepen.io/OFEII/full/qvYWxK',
 		demoImg:'https://s2.ax1x.com/2020/01/16/lxmU9e.png'
 	},
 	{
 		nr: '021',
 		name: 'text design',
-		link: 'https://codepen.io/FlorinPop17/full/JjPVBwx',
+		link: 'https://codepen.io/OFEII/full/rRbLbR',
 		demoImg:'https://s2.ax1x.com/2020/01/16/lxma1H.png'
 	},
 	{
 		nr: '022',
 		name: 'infinite scroll',
-		link: 'https://codepen.io/FlorinPop17/full/qBWGmqa',
+		link: 'https://codepen.io/OFEII/full/JzVXWM',
 		demoImg:'https://s2.ax1x.com/2020/01/16/lxmwjA.png'
 	},
 	{
 		nr: '023',
 		name: 'OOXX',
-		link: 'https://codepen.io/FlorinPop17/full/BaBePej',
+		link: 'https://codepen.io/OFEII/full/rRMvyW',
 		demoImg:'https://s2.ax1x.com/2020/01/16/lxmBnI.png'
 	},
 	{
 		nr: '024',
 		name: 'ghots mouse click',
-		link: 'https://codepen.io/FlorinPop17/full/ExYBNGy',
+		link: 'https://codepen.io/OFEII/full/wOKpyq',
 		demoImg:'https://s2.ax1x.com/2020/01/16/lxmDBt.png'
 	},
 	{
 		nr: '025',
 		name: 'color 🔺*n',
-		link: 'https://codepen.io/FlorinPop17/full/NWKQWmq',
+		link: 'https://codepen.io/OFEII/full/gyYQQq',
 		demoImg:'https://s2.ax1x.com/2020/01/16/lxmyAf.png'
 	},
 	{
 		nr: '026',
 		name: 'huawei p30 gradient-color',
-		link: 'https://codepen.io/FlorinPop17/full/eYOqQLz',
+		link: 'https://codepen.io/OFEII/full/KYKemo',
 		demoImg:'https://s2.ax1x.com/2020/01/16/lxm6N8.png'
 	},
 	{
 		nr: '027',
 		name: 'sun is the food of universe',
-		link: 'https://codepen.io/FlorinPop17/full/yLLBPLZ',
+		link: 'https://codepen.io/OFEII/full/zXPJGm',
 		demoImg:'https://s2.ax1x.com/2020/01/16/lxm29g.png'
 	},
 	{
 		nr: '028',
 		name: 'text-fill-color',
-		link: 'https://codepen.io/FlorinPop17/full/ExxYJdE',
+		link: 'https://codepen.io/OFEII/full/ewPYqV',
 		demoImg:'https://s2.ax1x.com/2020/01/16/lxmR3Q.png'
 	},
 	{
 		nr: '029',
 		name: 'long title',
-		link: 'https://codepen.io/FlorinPop17/full/eYYYErv',
+		link: 'https://codepen.io/OFEII/full/yLyNVxG',
 		demoImg:'https://s2.ax1x.com/2020/01/16/lxd8te.png'
 	},
 	{
 		nr: '030',
-		name: 'online weather',
-		link: 'https://codepen.io/FlorinPop17/full/NWWWYoe',
+		name: '实况天气',
+		link: 'https://codepen.io/OFEII/full/MxeXYr',
 		demoImg:'https://s2.ax1x.com/2020/01/16/lxdt1A.png'
 	},
 	{
 		nr: '031',
 		name: 'login -anima',
-		link: 'https://codepen.io/FlorinPop17/full/JjPVBwx',
+		link: 'https://codepen.io/OFEII/full/OJPVQox',
 		demoImg:'https://s2.ax1x.com/2020/01/16/lxd3kD.png'
 	},
 	{
 		nr: '032',
 		name: 'hover over me',
-		link: 'https://codepen.io/FlorinPop17/full/qBWGmqa',
+		link: 'https://codepen.io/OFEII/full/abbxVpe',
 		demoImg:'https://s2.ax1x.com/2020/01/16/lxdQ0K.png'
 	},
 	{
 		nr: '033',
-		name: 'star rate',
-		link: 'https://codepen.io/FlorinPop17/full/BaBePej',
+		name: 'star rate 星级评价',
+		link: 'https://codepen.io/OFEII/full/ZEzBLVw',
 		demoImg:'https://s2.ax1x.com/2020/01/16/lxdn61.png'
 	},
 	{
 		nr: '034',
 		name: 'fitch img',
-		link: 'https://codepen.io/FlorinPop17/full/ExYBNGy',
+		link: 'https://codepen.io/OFEII/full/VNKVQG',
 		demoImg:'https://s2.ax1x.com/2020/01/16/lxdmlR.png'
 	},
 	{
 		nr: '035',
 		name: 'icon-sunny',
-		link: 'https://codepen.io/FlorinPop17/full/NWKQWmq',
+		link: 'https://codepen.io/OFEII/full/bXGWmj',
 		demoImg:'https://s2.ax1x.com/2020/01/16/lxdep9.png'
 	},
 	{
 		nr: '036',
 		name: 'icon-cloudy',
-		link: 'https://codepen.io/FlorinPop17/full/eYOqQLz',
+		link: 'https://codepen.io/OFEII/full/bXGRvX',
 		demoImg:'https://s2.ax1x.com/2020/01/16/lxdEY4.png'
 	},
 	{
 		nr: '037',
 		name: 'like qq music',
-		link: 'https://codepen.io/FlorinPop17/full/yLLBPLZ',
+		link: 'https://codepen.io/OFEII/full/XLQXwM',
 		demoImg:'https://s2.ax1x.com/2020/01/16/lxdAkF.png'
 	},
 	{
 		nr: '038',
 		name: 'anim-cicrle',
-		link: 'https://codepen.io/FlorinPop17/full/ExxYJdE',
+		link: 'https://codepen.io/OFEII/full/zVJjde',
 		demoImg:'https://s2.ax1x.com/2020/01/16/lxdFTU.png'
 	},
 	{
 		nr: '039',
 		name: 'blibli btn',
-		link: 'https://codepen.io/FlorinPop17/full/eYYYErv',
+		link: 'https://ofeii.github.io/demo-senior/btn-reno/',
 		demoImg:'https://s2.ax1x.com/2020/01/16/lxdVfJ.png'
 	},
 	// {
 	// 	nr: '040',
 	// 	name: 'online weather',
-	// 	link: 'https://codepen.io/FlorinPop17/full/NWWWYoe',
+	// 	link: 'https://ofeii.github.io/demo-senior/menu-icon/',
 	// 	demoImg:'https://s2.ax1x.com/2020/01/16/lxdt1A.png'
 	// },
 ]
