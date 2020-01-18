f3f74ed89c689daa963d3eb51b6c5efddcad58c6
OFEII <2282252066@qq.com>
2020-01-18 13:38:31 +0800

    test commit

---
+++ main.js
@@ -0,0 +1,278 @@
+const projects =[
+	{
+		nr: '001',
+		name: 'login jump',
+		link: 'https://codepen.io/OFEII/pen/bXGWmj',
+		demoImg:'https://s2.ax1x.com/2020/01/16/lxmmhF.gif'
+	},
+	{
+		nr: '002',
+		name: '3d btn',
+		link: 'https://codepen.io/FlorinPop17/full/eYOPdER',
+		demoImg:'https://s2.ax1x.com/2020/01/16/lxmiXn.png'
+	},
+	{
+		nr: '003',
+		name: '3d flip btn',
+		link: 'https://codepen.io/FlorinPop17/full/GRKwmgK',
+		demoImg:'https://s2.ax1x.com/2020/01/16/lxejTf.png'
+	},
+	{
+		nr: '004',
+		name: 'email',
+		link: 'https://codepen.io/FlorinPop17/full/NWKELoq',
+		demoImg:'https://s2.ax1x.com/2020/01/16/lxmSfg.png'
+	},
+	{
+		nr: '005',
+		name: '渐变边框',
+		link: 'https://codepen.io/FlorinPop17/full/QWLzdrV',
+		demoImg:'https://s2.ax1x.com/2020/01/16/lxm9pQ.png'
+	},
+	{
+		nr: '006',
+		name: '登录界面',
+		link: 'https://codepen.io/FlorinPop17/full/gOYZxyE',
+		demoImg:'https://s2.ax1x.com/2020/01/16/lxmClj.jpg'
+	},
+	{
+		nr: '007',
+		name: 'box-div',
+		link: 'https://codepen.io/FlorinPop17/full/KKPbjwy',
+		demoImg:'https://s2.ax1x.com/2020/01/16/lxmP6s.png'
+	},
+	{
+		nr: '008',
+		name: '3d btn',
+		link: 'https://codepen.io/FlorinPop17/full/aboXgpP',
+		demoImg:'https://s2.ax1x.com/2020/01/16/lxmA00.png'
+	},
+	{
+		nr: '009',
+		name: 'mobilephone-ad',
+		link: 'https://codepen.io/FlorinPop17/full/rNBRYKZ',
+		demoImg:'https://s2.ax1x.com/2020/01/16/lxmup4.png'
+	},
+	{
+		nr: '010',
+		name: '天气',
+		link: 'https://codepen.io/FlorinPop17/full/RwbOwrm',
+		demoImg:'https://s2.ax1x.com/2020/01/16/lxmZkT.png'
+	},
+	{
+		nr: '011',
+		name: '购物清单',
+		link: 'https://codepen.io/FlorinPop17/full/JjPVBwx',
+		demoImg:'https://s2.ax1x.com/2020/01/16/lxmetU.png'
+	},
+	{
+		nr: '012',
+		name: 'btn',
+		link: 'https://codepen.io/FlorinPop17/full/qBWGmqa',
+		demoImg:'https://s2.ax1x.com/2020/01/16/lxmK1J.png'
+	},
+	{
+		nr: '013',
+		name: 'class',
+		link: 'https://codepen.io/FlorinPop17/full/BaBePej',
+		demoImg:'https://s2.ax1x.com/2020/01/16/lxmMc9.png'
+	},
+	{
+		nr: '014',
+		name: 'fluteer water',
+		link: 'https://codepen.io/FlorinPop17/full/ExYBNGy',
+		demoImg:'https://s2.ax1x.com/2020/01/16/lxm1n1.png'
+	},
+	{
+		nr: '015',
+		name: 'switch btn',
+		link: 'https://codepen.io/FlorinPop17/full/NWKQWmq',
+		demoImg:'https://s2.ax1x.com/2020/01/16/lxmYtO.png'
+	},
+	{
+		nr: '016',
+		name: 'dm screen',
+		link: 'https://codepen.io/FlorinPop17/full/eYOqQLz',
+		demoImg:'https://s2.ax1x.com/2020/01/16/lxm30x.png'
+	},
+	{
+		nr: '017',
+		name: 'color picker',
+		link: 'https://codepen.io/FlorinPop17/full/yLLBPLZ',
+		demoImg:'https://s2.ax1x.com/2020/01/16/lxm876.png'
+	},
+	{
+		nr: '018',
+		name: 'color-cicrle',
+		link: 'https://codepen.io/FlorinPop17/full/ExxYJdE',
+		demoImg:'https://s2.ax1x.com/2020/01/16/lxmJAK.png'
+	},
+	{
+		nr: '019',
+		name: 'diamond',
+		link: 'https://codepen.io/FlorinPop17/full/eYYYErv',
+		demoImg:'https://s2.ax1x.com/2020/01/16/lxmthD.png'
+	},
+	{
+		nr: '020',
+		name: 'OFEII Seaarch in wiki',
+		link: 'https://codepen.io/FlorinPop17/full/NWWWYoe',
+		demoImg:'https://s2.ax1x.com/2020/01/16/lxmU9e.png'
+	},
+	{
+		nr: '021',
+		name: 'text design',
+		link: 'https://codepen.io/FlorinPop17/full/JjPVBwx',
+		demoImg:'https://s2.ax1x.com/2020/01/16/lxma1H.png'
+	},
+	{
+		nr: '022',
+		name: 'infinite scroll',
+		link: 'https://codepen.io/FlorinPop17/full/qBWGmqa',
+		demoImg:'https://s2.ax1x.com/2020/01/16/lxmwjA.png'
+	},
+	{
+		nr: '023',
+		name: 'OOXX',
+		link: 'https://codepen.io/FlorinPop17/full/BaBePej',
+		demoImg:'https://s2.ax1x.com/2020/01/16/lxmBnI.png'
+	},
+	{
+		nr: '024',
+		name: 'ghots mouse click',
+		link: 'https://codepen.io/FlorinPop17/full/ExYBNGy',
+		demoImg:'https://s2.ax1x.com/2020/01/16/lxmDBt.png'
+	},
+	{
+		nr: '025',
+		name: 'color 🔺*n',
+		link: 'https://codepen.io/FlorinPop17/full/NWKQWmq',
+		demoImg:'https://s2.ax1x.com/2020/01/16/lxmyAf.png'
+	},
+	{
+		nr: '026',
+		name: 'huawei p30 gradient-color',
+		link: 'https://codepen.io/FlorinPop17/full/eYOqQLz',
+		demoImg:'https://s2.ax1x.com/2020/01/16/lxm6N8.png'
+	},
+	{
+		nr: '027',
+		name: 'sun is the food of universe',
+		link: 'https://codepen.io/FlorinPop17/full/yLLBPLZ',
+		demoImg:'https://s2.ax1x.com/2020/01/16/lxm29g.png'
+	},
+	{
+		nr: '028',
+		name: 'text-fill-color',
+		link: 'https://codepen.io/FlorinPop17/full/ExxYJdE',
+		demoImg:'https://s2.ax1x.com/2020/01/16/lxmR3Q.png'
+	},
+	{
+		nr: '029',
+		name: 'long title',
+		link: 'https://codepen.io/FlorinPop17/full/eYYYErv',
+		demoImg:'https://s2.ax1x.com/2020/01/16/lxd8te.png'
+	},
+	{
+		nr: '030',
+		name: 'online weather',
+		link: 'https://codepen.io/FlorinPop17/full/NWWWYoe',
+		demoImg:'https://s2.ax1x.com/2020/01/16/lxdt1A.png'
+	},
+	{
+		nr: '031',
+		name: 'login -anima',
+		link: 'https://codepen.io/FlorinPop17/full/JjPVBwx',
+		demoImg:'https://s2.ax1x.com/2020/01/16/lxd3kD.png'
+	},
+	{
+		nr: '032',
+		name: 'hover over me',
+		link: 'https://codepen.io/FlorinPop17/full/qBWGmqa',
+		demoImg:'https://s2.ax1x.com/2020/01/16/lxdQ0K.png'
+	},
+	{
+		nr: '033',
+		name: 'star rate',
+		link: 'https://codepen.io/FlorinPop17/full/BaBePej',
+		demoImg:'https://s2.ax1x.com/2020/01/16/lxdn61.png'
+	},
+	{
+		nr: '034',
+		name: 'fitch img',
+		link: 'https://codepen.io/FlorinPop17/full/ExYBNGy',
+		demoImg:'https://s2.ax1x.com/2020/01/16/lxdmlR.png'
+	},
+	{
+		nr: '035',
+		name: 'icon-sunny',
+		link: 'https://codepen.io/FlorinPop17/full/NWKQWmq',
+		demoImg:'https://s2.ax1x.com/2020/01/16/lxdep9.png'
+	},
+	{
+		nr: '036',
+		name: 'icon-cloudy',
+		link: 'https://codepen.io/FlorinPop17/full/eYOqQLz',
+		demoImg:'https://s2.ax1x.com/2020/01/16/lxdEY4.png'
+	},
+	{
+		nr: '037',
+		name: 'like qq music',
+		link: 'https://codepen.io/FlorinPop17/full/yLLBPLZ',
+		demoImg:'https://s2.ax1x.com/2020/01/16/lxdAkF.png'
+	},
+	{
+		nr: '038',
+		name: 'anim-cicrle',
+		link: 'https://codepen.io/FlorinPop17/full/ExxYJdE',
+		demoImg:'https://s2.ax1x.com/2020/01/16/lxdFTU.png'
+	},
+	{
+		nr: '039',
+		name: 'blibli btn',
+		link: 'https://codepen.io/FlorinPop17/full/eYYYErv',
+		demoImg:'https://s2.ax1x.com/2020/01/16/lxdVfJ.png'
+	},
+	// {
+	// 	nr: '040',
+	// 	name: 'online weather',
+	// 	link: 'https://codepen.io/FlorinPop17/full/NWWWYoe',
+	// 	demoImg:'https://s2.ax1x.com/2020/01/16/lxdt1A.png'
+	// },
+]
+
+const app = document.getElementById('app');
+const imgBaseURL = 'https://www.florin-pop.com/images/100Days100PRojects/Day ';
+
+projects.forEach(project => {
+	const title = `${project.name} - #${project.nr}`;
+	const imgURL = imgBaseURL + project.nr + '.jpg';
+	
+	const projectEl = document.createElement('a');
+	projectEl.classList.add('project');
+	projectEl.href = project.link;
+	projectEl.target = '_blank';
+	const imgURLNew = 'https://s2.ax1x.com/2020/01/16/ljjhWD.jpg'
+
+	projectEl.innerHTML = `
+        <img src="${project.demoImg}" alt="${title}"/>
+		<p>
+			${title}
+		</p>
+    `;
+
+	app.appendChild(projectEl);
+});
+
+// SOCIAL PANEL JS
+const floating_btn = document.querySelector('.floating-btn');
+const close_btn = document.querySelector('.close-btn');
+const social_panel_container = document.querySelector('.social-panel-container');
+
+floating_btn.addEventListener('click', () => {
+	social_panel_container.classList.toggle('visible')
+});
+
+close_btn.addEventListener('click', () => {
+	social_panel_container.classList.remove('visible')
+});
